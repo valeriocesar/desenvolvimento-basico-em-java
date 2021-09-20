b2d29fe9c1cafd46118e9e7b3a7111992258aff0
valeriocesar <valerio.cesar@gmail.com>
2021-09-20 19:15:07 -0300

    add README

--- src/main/java/ClasseDate/comparacaoData.java
+++ src/main/java/ClasseDate/comparacaoData.java
@@ -3,18 +3,29 @@ package ClasseDate;
 import java.util.Date;
 
 public class comparacaoData {
+
     public static void main(String[] args) {
 
-        Date dataNascimento = new Date(581137200000L);
-        System.out.println(dataNascimento);
+        Date dataNascimento = new Date(581137200000L); //01/06/1988
+        System.out.println("A data de nascimento é: " +dataNascimento);
 
-        Date dataComparacao = new Date(1299294000000L);
+        Date dataComparacao = new Date(1275361200000L); //01/06/2010
+        System.out.println("A data de comparação é: " +dataComparacao);
 
         boolean isAfter = dataComparacao.after(dataNascimento);
-        System.out.println(isAfter);
 
-        boolean isBefore = dataComparacao.before(dataNascimento);
-        System.out.println(isBefore);
+        if (isAfter) {
+            System.out.println("A data de comparação é após a data de nascimento.");
+        } else {
+            System.out.println("A data de comparação é anterior a data de nascimento.");
+        }
+
+//        boolean isAfter = dataComparacao.after(dataNascimento);
+//        System.out.println("A data de comparação é após a data de nascimento.");
+//
+//        boolean isBefore = dataComparacao.before(dataNascimento);
+//        System.out.println("A data de nascimento é anterior a data de comparação.");
 
     }
+
 }
