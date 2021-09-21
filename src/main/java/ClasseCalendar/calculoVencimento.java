7ed550d0e524ee536e71cbd08ed742c96d992be2
valeriocesar <valerio.cesar@gmail.com>
2021-09-21 17:47:59 -0300

    Melhorias no calculoVencimento

--- src/main/java/ClasseCalendar/calculoVencimento.java
+++ src/main/java/ClasseCalendar/calculoVencimento.java
@@ -7,23 +7,26 @@ public class calculoVencimento {
 
     public static void main(String[] args) {
 
-        SimpleDateFormat data = new SimpleDateFormat("EEEE dd MMMM yyyy");
+        SimpleDateFormat data = new SimpleDateFormat("EEEE, dd 'de' MMMM 'de' yyyy.");
 
         Calendar emissao = Calendar.getInstance();
         emissao.set(2021, Calendar.SEPTEMBER, 20);
 
-        System.out.println("O documento foi emitido " + data.format(emissao.getTime()) );
+        System.out.println("O documento foi emitido " + data.format(emissao.getTime()));
 
-        emissao.add (Calendar.DATE, 20);
+        emissao.add(Calendar.DATE, 20);
 
         if (emissao.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) {
-            System.out.println("Seu vencimento é " + data.format(emissao.getTime()) + ", portando pague no próximo dia útil." );
+            emissao.add(Calendar.DATE, 2);
+            System.out.println("Efetue o pagamento até " + data.format(emissao.getTime()));
         }
-        if (emissao.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
-            System.out.println("Seu vencimento é " + data.format(emissao.getTime()) + ", portando pague no próximo dia útil." );
-    } else {
-            System.out.println("Seu vencimento é " + data.format(emissao.getTime()));
+        else if (emissao.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+            emissao.add(Calendar.DATE, 1);
+            System.out.println("Efetue o pagamento até " + data.format(emissao.getTime()));
+        } else {
+            System.out.println("Efetue o pagamento até " + data.format(emissao.getTime()));
         }
+
     }
 
 }
